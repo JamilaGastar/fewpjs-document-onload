148899fd9739f1a571f044264240a31c44f9e676
jamilagastar <jamilagastar@gmail.com>
2022-05-01 20:07:28 +1000

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Your code goes here
+
+// document.addEventListener("DOMContentLoaded", function() {
+//     console.log("The DOM has loaded");
+//   });
+
+//   console.log(
+//     "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+//   );
+
+document.addEventListener("DOMContentLoaded", function() {
+    const modifyText = document.getElementById('text');
+
+    modifyText.innerHTML = "This is really cool!";
+});
